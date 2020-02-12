6c0d1c07555c439e02b0d6c17a15ed3e64dfe50c
Zoko11 <d.towers2@ncl.ac.uk>
2020-02-12 16:44:36 +0000

    Added in files that are part of the component system

---
+++ project/ComponentSystem/Component.cpp
@@ -0,0 +1,22 @@
+#include "Component.h"
+
+Component::Component() {
+	this->name = "";
+}
+
+Component::Component(std::string name) {
+	this->name = name;
+}
+
+std::string Component::getName() {
+	return name;
+}
+void Component::setName(std::string name) {
+	this->name = name;
+}
+
+void Component::Start() {}
+
+void Component::Update() {}
+
+void Component::LateUpdate() {}
---
+++ project/ComponentSystem/Scene.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <vector>
+#include "GameObject.h"
+
+class Scene {
+private:
+	std::vector<GameObject*> objects;
+
+public:
+	Scene() {
+
+	}
+
+	Scene(const Scene& S) {
+		*this = S;
+	}
+
+	Scene& operator=(Scene S) {
+		for (GameObject* i : S.objects) {
+			objects.emplace_back(i);
+		}
+		return *this;
+	}
+
+	~Scene() {
+		for (GameObject* i : objects) {
+			delete i;
+			i = nullptr;
+		}
+	}
+
+	void addGameObject(GameObject* go) {
+		objects.emplace_back(go);
+	}
+
+	void Start() {
+		for (GameObject* i : objects) {
+			i->Start();
+		}
+	}
+
+	void Update() {
+		for (GameObject* i : objects) {
+			i->Update();
+		}
+	}
+
+	void LateUpdate() {
+		for (GameObject* i : objects) {
+			i->LateUpdate();
+		}
+	}
+};
