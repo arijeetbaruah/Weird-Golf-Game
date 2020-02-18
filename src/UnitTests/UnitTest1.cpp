f127a9578a7f0d8e2483a6da17e8e44bed10e14f
JustinCar <carterjustin67@gmail.com>
2020-02-18 16:11:39 +0000

    Quick test

--- src/UnitTests/UnitTest1.cpp
+++ src/UnitTests/UnitTest1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cassert>
 
+#include "../../include/Common/Vector2.h"
+
 int main() {
-	assert(true);
+	Vector2 vec = new Vector2(3, 4);
+	assert((vec.x == 3) && (vec.y == 4));
 }
