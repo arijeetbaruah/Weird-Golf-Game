30c968d9bd3246b9fdfbc46a033f01716c98da8c
JustinCar <carterjustin67@gmail.com>
2020-02-18 16:46:10 +0000

    test jenkins

--- src/UnitTests/UnitTest1.cpp
+++ src/UnitTests/UnitTest1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cassert>
 
-#include "../../include/Common/Vector2.h"
+#include "..//../include/Common/Vector2.h"
+using namespace NCL::Maths;
 
 int main() {
-	Vector2 vec = new Vector2(3, 4);
-	assert((vec.x == 3) && (vec.y == 4));
+	Vector2* vec = new Vector2(3, 4);
+	assert((vec->x == 3) && (vec->y == 4));
 }
