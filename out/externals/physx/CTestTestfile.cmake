1b34b39776a6abdf8b7e4c16c27b048e9ad61196
JustinCar <carterjustin67@gmail.com>
2020-02-19 10:56:37 +0000

    commit

---
+++ out/externals/physx/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/b6038386/Documents/GitHub/weird-golf-game/submodules/PhysX/physx/compiler/public
+# Build directory: C:/Users/b6038386/Documents/GitHub/weird-golf-game/out/externals/physx
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("sdk_source_bin")
---
+++ out/externals/physx/sdk_source_bin/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/b6038386/Documents/GitHub/weird-golf-game/submodules/PhysX/physx/source/compiler/cmake
+# Build directory: C:/Users/b6038386/Documents/GitHub/weird-golf-game/out/externals/physx/sdk_source_bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
