3d3b2a32059646d90478c8b260bfa0af430a8364
David Towers <Towers.D27@gmail.com>
2020-02-20 15:06:41 +0000

    Updated getcomponent

--- src/UnitTests/ComponentTest.cpp
+++ src/UnitTests/ComponentTest.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cassert>
 #include "ComponentSystem/GameObject.h"
+#include "ComponentSystem/Transform.h"
 
 #define ASSERT_WITH_FAILEXIT(COND) do {if (!(COND)) exit(-1);} while(0)
 
 int main() {
 	GameObject go = GameObject();
-	Component* tf = go.getComponent("Transform");
-	ASSERT_WITH_FAILEXIT(tf != nullptr && tf->getName() == "Tranform");
+	Transform* tf = go.getComponent<Transform*>("Transform");
+	ASSERT_WITH_FAILEXIT(tf != nullptr && tf->getName() == "Transform");
 }
