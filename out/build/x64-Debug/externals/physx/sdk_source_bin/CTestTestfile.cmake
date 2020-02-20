e37146ea6598a88a314cf20ee935b5fdfa0518e2
arijeetbaruah <arijeetbaruah@gmail.com>
2020-02-20 14:19:49 +0000

    out delete

--- out/build/x64-Debug/externals/physx/sdk_source_bin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/b6024306/source/Group Work/weird-golf-game/submodules/PhysX/PhysX/source/compiler/cmake
-# Build directory: C:/Users/b6024306/source/Group Work/weird-golf-game/out/build/x64-Debug/externals/physx/sdk_source_bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/x64-Debug/submodules/enet/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/b6024306/source/Group Work/weird-golf-game/submodules/enet
-# Build directory: C:/Users/b6024306/source/Group Work/weird-golf-game/out/build/x64-Debug/submodules/enet
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
