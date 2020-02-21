866a96b6e99d1bf3ecfa9d4350806b7731a95524
David Towers <Towers.D27@gmail.com>
2020-02-21 14:21:00 +0000

    Added methods to retrieve position and rotation from transform

--- src/UnitTests/ComponentTest.cpp
+++ src/UnitTests/ComponentTest.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cassert>
 #include "ComponentSystem/GameObject.h"
+#include "Common/Matrix4.h"
 #include "ComponentSystem/Transform.h"
 
 #define ASSERT_WITH_FAILEXIT(COND) do {if (!(COND)) exit(-1);} while(0)
 
 int main() {
-	GameObject go = GameObject();
+	Matrix4 mat = Matrix4::Rotation(30, Vector3(1, 0, 0)) * Matrix4::Rotation(40, Vector3(0, 1, 0)) * Matrix4::Rotation(50, Vector3(0,0,1));
+	GameObject go = GameObject(mat);
 	Transform* tf = go.getComponent<Transform*>("Transform");
 	ASSERT_WITH_FAILEXIT(tf != nullptr && tf->getName() == "Transform");
+	std::cout << tf->getRotationVec() << std::endl;
 }
