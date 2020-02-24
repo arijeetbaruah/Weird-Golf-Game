c7c374cdb039acc5ab2a0753394c579acfb54c5a
Christopher Cometto <christopher.cometto@hotmail.it>
2020-02-24 17:19:37 +0000

    test for triangleMesh

--- Game_Tech/CSC8503/GameTech/TutorialGame.cpp
+++ Game_Tech/CSC8503/GameTech/TutorialGame.cpp
@@ -156,6 +156,22 @@ void TutorialGame::InitialiseAssets() {
 	objLoadLevelFunc("Assets/TestLevel.obj");
 	objLoadFunc("Assets/Ball.obj", &playerMesh);
 
+	std::vector<PxVec3> verts;
+	std::vector<PxU32> tris;
+
+	for each(OGLMesh* mesh in golfLevelMeshes) {
+		for each (Vector3 vert in mesh->GetPositionData()) {
+			verts.push_back(PxVec3(vert.x, vert.y, vert.z));
+		}
+		for each (unsigned int index in mesh->GetIndexData()) {
+			tris.push_back(index);
+		}
+		physxC.addTriangleMeshToScene(verts, tris);
+		verts.clear();
+		tris.clear();
+	}
+	physxC.spawnBall();
+	
 	basicTex	= (OGLTexture*)TextureLoader::LoadAPITexture("checkerboard.png");
 	basicShader = new OGLShader("GameTechVert.glsl", "GameTechFrag.glsl");
 }
