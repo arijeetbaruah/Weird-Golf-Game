27dd0ce30c057efa36b4609cf5efc1ea73d48821
Carrie4915 <56798367+Carrie4915@users.noreply.github.com>
2020-02-28 14:03:15 +0000

    AudioManager

--- Game_Tech/Plugins/AudioManager/AudioManager.cpp
+++ Game_Tech/Plugins/AudioManager/AudioManager.cpp
@@ -1,1 +1,16 @@
+#include<iostream>
 #include "AudioManager.h"
+#include "Fmod.hpp"
+#include "Fmod_errors.h"
+#include "Fmod.h"
+using namespace FMOD;
+typedef Sound* SoundClass;
+
+class SoundSystemClass 
+{
+	System* pSystem;
+	SoundSystemClass() {}
+};
+
+
+
